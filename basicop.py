2c72a919c54e4c6cb9e98b8c54a52c73872a8333
Abhinandan Guruprasad  Madiwal <163436368+Abhinandan999@users.noreply.github.com>
2024-07-18 23:36:23 +0530

    Maths basic

---
+++ basicop.py
@@ -0,0 +1,19 @@
+a=int(input("Enter a:-"))
+b=int(input("Enter b:-"))
+
+c=a+b
+d=a-b
+g=a*b
+e=a/b
+f=a%b
+
+print("Sum is              " )
+print(c)
+print("\nSubstraction is   " )
+print(d)
+print("\nMultiplication is " )
+print(g)
+print("\nDivide is         " )
+print(e)
+print("\nModulas operation is ")
+print(f)
